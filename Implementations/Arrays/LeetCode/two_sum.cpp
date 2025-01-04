0a9c7fff4736c615756da2b92dec9acee5b62d98
Fábio Kochem <97361731+fabiokochem@users.noreply.github.com>
2025-01-04 23:31:17 +0100

    Update two_sum.cpp

--- Implementations/Arrays/LeetCode/two_sum.cpp
+++ Implementations/Arrays/LeetCode/two_sum.cpp
@@ -18,19 +18,15 @@ Solve the problem but is a bad solution, O(n²).
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> arr(2);
-
         for(int i = 0; i < nums.size(); i++){
             for(int j = i + 1; j < nums.size(); j++){
                 if(nums[i] + nums[j] == target){
-                    arr[0] = i;
-                    arr[1] = j;
-                    return arr;
+                    return {i, j};
                 }
             }
         }
 
-        return arr;
+        return {};
     }
 };
 
